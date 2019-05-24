a13573627a97abddd78b3c7c8126aa93b12c6bb9
nleavi11 <noahjlvtt@gmail.com>
2019-05-23 21:04:13 -0600

    added changes

--- script.js
+++ script.js
@@ -2,12 +2,9 @@ let app = new Vue({
   el: '#app',
   data: {
     search: '',
-    searches: ["amen to that"],
-    current: {
-      title: '',
-      img: '',
-      alt: ''
-    },
+    pictures: [],
+    searches: ["lord of the rings"],
+    current: { },
     loading: true,
     searched_book: '',
   },
@@ -25,8 +22,22 @@ let app = new Vue({
         this.loading = true;
 
         const response = await axios.get(url);
-        console.log("response: ", response);
         this.current = response.data;
+        console.log("before: ", this.current.docs.length);
+        for(i = 0; i < this.current.docs.length; i++){
+          if(this.current.docs[i].hasOwnProperty('isbn')){
+             picture = "https://openlibrary.org/api/books?bibkeys=ISBN:" + this.current.docs[i].isbn[0] + "&jscmd=details&format=json";
+             const response_picture = await axios.get(picture);
+             console.log(response_picture.thumbnail_url);
+             this.current.docs[i].img = response_picture.thumbnail_url;
+             continue;
+          }
+          else{
+            console.log("deleted")
+            this.current.docs.splice(i);
+          }
+        }
+        console.log("after: ", this.current.docs.length)
         this.search = '';
         this.loading = false;
         return true;
@@ -41,11 +52,5 @@ let app = new Vue({
       this.current = {};
       this.book_search();
     },
-    keep_book(title, isbn, author, date) {
-      if(title.length === 0 || isbn.length === 0 || author.length === 0 || date.length === 0){
-        return false;
-      }
-      return true;
-    }
   },
 })
